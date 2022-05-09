62f16530e0d9b62e14cbb5e89b5c0e845ede8829
Bram Lo <bram_lo@outlook.com>
2022-05-09 22:35:44 +0200

    Add confusion matrix plot
    Use test_set to validate models performance; create a confusion matrix
    and use accuracy of test set as metric instead of last val_accuracy.

--- src/train.py
+++ src/train.py
@@ -1,12 +1,15 @@
 import json
 
 from keras.callbacks import CSVLogger
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 import yaml
+from config import DATASET_PATH
 
-from data import train_ds, val_ds
+from data import test_ds, train_ds, val_ds
 from model import build_model
+from utils import get_commands, save_as_matrix_csv
 
 csv_logger = CSVLogger('log.csv', append=False, separator=',')
 model = build_model()
@@ -31,5 +34,22 @@ df = pd.read_csv("log.csv")
 loss_df = df[["loss"]]
 loss_df.to_csv("loss.csv", index=False)
 
+test_audio = []
+test_labels = []
+
+for audio, label in test_ds:
+    test_audio.append(audio.numpy())
+    test_labels.append(label.numpy())
+
+test_audio = np.array(test_audio)
+test_labels = np.array(test_labels)
+
+y_pred = np.argmax(model.predict(test_audio), axis=1)
+y_true = test_labels
+
+save_as_matrix_csv(y_true, y_pred, get_commands(DATASET_PATH))
+
+test_acc = sum(y_pred == y_true) / len(y_true)
+
 with open("metrics.json", 'w') as outfile:
-    json.dump({"accuracy": df.iloc[-1]["val_accuracy"]}, outfile)
+    json.dump({"accuracy": test_acc}, outfile)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
+from typing import Any
 import numpy as np
 import tensorflow as tf
+import pandas as pd
 
 
 def get_commands(data_dir):
@@ -15,3 +17,31 @@ def get_commands(data_dir):
     commands = np.array(tf.io.gfile.listdir(str(data_dir)))
     commands = commands[commands != 'README.md']
     return commands
+
+
+def save_as_matrix_csv(actual: 'np.ndarray[Any]', predicted: 'np.ndarray[Any]',
+                       commands: list[str]) -> None:
+    """save_as_matrix_csv.
+
+    Helper function to save a confusion matrix to be used by CML + DVC in an
+    ML-pipeline as a CSV file.
+
+    Args:
+        actual: true labels
+        predicted: predicted labels
+
+    Returns:
+        None:
+    """
+    # first copy actual and predicted as stringn ndarrays
+    actual = actual.copy().astype(str)
+    predicted = predicted.copy().astype(str)
+
+    # Replace int labels with actual names in both actual & predicted
+    for i in range(len(commands)):
+        actual[actual == str(i)] = commands[i]
+        predicted[predicted == str(i)] = commands[i]
+
+    df = pd.DataFrame({"actual": actual, "predicted": predicted})
+
+    df.to_csv("matrix.csv", index=False)
