80037daddd6def0c632ab7adb99054e559570470
Bram Lo <bram_lo@outlook.com>
2022-05-13 18:59:38 +0200

    Add tts generator
    Generator class that generates mp3 samples from https://lazypy.ro/tts/
    using different speakers. Used for creating custom dataset used in
    manual testing.

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
+import json
 from typing import Any
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import requests
 import seaborn as sns
 import tensorflow as tf
 
@@ -69,3 +71,46 @@ def save_plots_as_csv(metrics):
                                                                 index=False)
     pd.DataFrame(data=metrics["val_loss"],
                  columns=["val_loss"]).to_csv('val_loss.csv', index=False)
+
+
+class TTSCommandGenerator:
+    """
+    Generator object to create speech files from https://lazypy.ro/tts/ for
+    manual testing.
+    """
+
+    def __init__(
+        self,
+        service="Polly",
+        host="https://lazypy.ro/",
+        speakers=[
+            "Brian", "Amy", "Emma", "Geraint", "Russel", "Nicole", "Joey",
+            "Justin", "Matthew", "Ivy", "Joanna", "Kendra", "Kimberly", "Salli"
+        ]
+    ) -> None:
+        self.service = service
+        self.host = host
+        self.speakers = speakers
+
+    def generate_tts(self, command, path):
+        """generate_tts.
+
+        Generate a text to speech file and save it to disk
+
+        Args:
+            command: text to speak by speaker
+            path: dir to save all tts files
+        """
+        for speaker in self.speakers:
+            payload = {
+                "service": self.service,
+                "voice": speaker,
+                "text": command
+            }
+
+            res = requests.post(self.host + "tts/proxy.php", data=payload)
+            url = res.json()['speak_url']
+            doc = requests.get(url)
+
+            with open(f"{path}/{command}_{speaker}.mp3", "wb") as f:
+                f.write(doc.content)
